317f4b35b719508486dae20342b2c6db9e7cc176
Pal Szentgyoergyi <pszentg@gmail.com>
2020-06-01 19:51:53 +0200

    fixed 62px borders so the image is 224x224 (ideal for resnet). returning images as torch expects them

--- app/preprocess.py
+++ app/preprocess.py
@@ -3,23 +3,36 @@ from random import randrange
 
 import cv2
 import pandas
+import torch
 from imageio import imread
 from tqdm import tqdm
 
-data = pandas.read_csv('../data/bees/bee_data.csv')
-data.head()
-train_img = []
-for img_name in tqdm(data['file']):
-    image_path = '../data/bees/bee_imgs/bee_imgs/' + img_name
-    img = imread(image_path)
-    img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
-    h, w, c = img.shape
-    print(img.shape)
-    rh = randrange(0, h - 100)
-    rw = randrange(0, w - 100)
-    cropped = cv2.copyMakeBorder(img[rw:rw + 100, rh:rh + 100], h - (rh + 100), rh, rw, w - (rw + 100),
-                                 borderType=cv2.BORDER_CONSTANT)
-    # cv2.imshow('cropped', cropped)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    train_img.append(cropped)
+
+def preprocess(image_data_path='../data/bees/bee_imgs/bee_imgs/', csv_path='../data/bees/bee_data.csv'):
+    data = pandas.read_csv(csv_path)
+    data.head()
+    train_img = []
+    # this could be done differently if I didn't have a dataset with classifications
+    for img_name in tqdm(data['file']):
+        image_path = image_data_path + img_name
+        img = imread(image_path)
+        img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)
+        h, w, c = img.shape
+        try:
+            rh = randrange(0, h - 100)
+        except ValueError:
+            rh = 0
+        try:
+            rw = randrange(0, w - 100)
+        except ValueError:
+            rw = 0
+
+        crop = img[rw:rw + 100, rh:rh + 100]
+        cropped = cv2.copyMakeBorder(crop, 62, 62, 62, 62,
+                                     borderType=cv2.BORDER_CONSTANT)
+        # cv2.imshow('cropped', cropped)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        train_img.append(torch.from_numpy(cropped))
+
+    return train_img
